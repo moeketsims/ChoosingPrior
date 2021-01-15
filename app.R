d771814bdb92319949b00a149f51ca709db8743f
moeketsims <bricoleurmosia@gmail.com>
2021-01-15 19:55:15 +0200

    UI styling changes

--- app.R
+++ app.R
@@ -1,13 +1,35 @@
-source("utils/modules_script.R")
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(shinyWidgets)
+
+
 DistplotApp <- function(){
+  source("utils/modules_script.R")
+  library(shiny)
+  library(ggplot2)
+  library(dplyr)
+  library(shinyWidgets)
+  require(graphics)
+  
   ui <- fluidPage(
-    headerPanel(h1("Probabilistic Machine Learning Models", align = "center")),
-    titlePanel("Beta(a,b)"),
-    HistoramUI("hist1")
+    
+    includeCSS("www/styles.css"),
+    #img(src='www/BYS.PNG', align = "right"),
+    withMathJax(),
+    titlePanel(tags$i(
+      h1(strong("Probabilistic Machine Learning: Assuming Prior of Parameters"), 
+         style = "font-family: 'times'; font-size: 25px; text-align: center")
+    )),
+    align = "center",
+    navbarPage(
+      theme = "https://stackpath.bootstrapcdn.com/bootswatch/3.4.1/flatly/bootstrap.min.css",
+      title = 'Choosing a Priors',
+      tabPanel('Beta Prior',
+               HistoramUI("hist1")),
+       tabPanel('Dirichlet Prior',
+                h1("To be added")),
+      tabPanel('Gaussian Prior',
+               h1("To be added")
+               )
+    ),
+    
   )
   server <- function(input, output, session){
     HistoramServer("hist1")
--- utils/modules_script.R
+++ utils/modules_script.R
@@ -1,21 +1,31 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
 HistoramUI <- function(id){
   tagList(
     sidebarLayout(
-      sidebarPanel(width = 4,
+      sidebarPanel(width = 2,
         fluidRow(
           column(12,
-                 chooseSliderSkin("Modern", color = "#112446"),
+                 chooseSliderSkin("Modern"),
                  sliderInput(NS(id, "a"), "Select: a", min = 0, max = 10, value = 1, step = 1),
                  sliderInput(NS(id, "b"), "Select: b", min = 0, max = 10, value = 1, step = 1)
           )
         )
       ),
-      mainPanel(
-        column(6,
+      mainPanel(width = 10,
+        column(5,
                plotOutput(NS(id, "distplot"))
+               ),
+        
+        column(7,
+               helpText('In the Bayesian Machine learning (BML) framework, we use subjective probability to quantify/encode 
+               our belief about the world before seeing evidence(data). 
+               For instance, when modelling a binary outcome data = \\(\\{0,1\\}_{i=1}^{n}\\), we assume a 
+               Bernoulli likelihood Bern(\\(\\theta\\)) where \\(\\theta\\) is a probability of success 
+               (e.g. completed a task denoted by 1 and not completed by 0). Thus, in BML we acknowledge that, we know something about the world we are model. 
+               For example, we may believe that students are likely to complete a qualification and thus, this prior 
+               belief can be quantified by the Beta(a,b) density plot on the left. This belief can be seen by choosing a value of  
+               \\(a > b\\)
+                  ')
+
                )
       )
 
@@ -34,8 +44,9 @@ HistoramServer <- function(id){
                  output$distplot <- renderPlot({
                    data() %>% ggplot(aes(x, y))+
                      geom_line()+
-                     labs(x="theta", y = "Density", title = paste0("Beta(",input$a,",",input$b,")"))+
+                     labs(x=bquote(~ theta), y = "Density", title = paste0("Beta(",input$a,",",input$b,")"))+
                      theme_light()+
+                     theme(axis.title.x = element_text(size = 20))+
                      theme(plot.title = element_text(hjust = 0.5))
                    })
                }
