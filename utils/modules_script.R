bf19ae381f5fdf66e6d0e3fbec3cb1a8e454901e
moeketsims <bricoleurmosia@gmail.com>
2021-01-17 01:16:13 +0200

    changed Modules

--- utils/modules_script.R
+++ utils/modules_script.R
@@ -1,24 +1,36 @@
 
-SliderUI <- function(id){
-  tagList(
-    chooseSliderSkin("Modern"),
-    sliderInput(NS(id,"a"), "Select: a", min = 0, max = 10, value = 1),
-    sliderInput(NS(id,"b"), "Select: b", min = 0, max = 10, value = 1)
-  )
+DataInput <- function(id){
+ tagList(
+   numericInput(NS(id, "sample_size"), label = "Enter Sample Size:", value = 100),
+   numericInput(NS(id, "p"), label = "Enter Parameter:", value = 0.5),
+   actionBttn(NS(id, "gen"), label = "Simulate")
+ )
 }
 
 
-SliderServer <- function(id){
+DataServer <- function(id){
   moduleServer(id,
                function(input, output, session){
-                 data <- reactive({
-                   x <- seq(0,1,length=200)
-                   y <- dbeta(x, input$a, input$b)
-                   df <- data.frame(x=x, y=y)
-                   return(df) 
-                   })
-
+                 df <- reactive({
+                    df <- rbernoulli(input$sample_size, input$p)
+                    df <- data.frame(a=1:input$sample_size, b=df)
+                    return(df)
+                 })
                }
-             )
+               )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
