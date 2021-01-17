f5492e54d7b6e9a9193c9c08f82894c3e07f0fd5
moeketsims <36858306+moeketsims@users.noreply.github.com>
2021-01-17 22:25:37 +0200

    Update run.R

--- run.R
+++ run.R
@@ -1,4 +1,12 @@
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(shinyWidgets)
+library(graphics)
+library(purrr)
+library(DT)
+library(shinydashboard)
+
 port <- Sys.getenv('PORT')
 shiny::runApp(
   appDir = getwd(),
